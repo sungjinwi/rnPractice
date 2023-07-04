6d4257b669eb14c7649a49a21829b0df6e9aa8aa
sungjinwi <0202wsj@gmail.com>
2023-07-05 00:53:59 +0900

    FIX : people array changed from constant to state

--- src/components/TabOneScreen.tsx
+++ src/components/TabOneScreen.tsx
@@ -6,21 +6,9 @@ import {
   FlatList,
   StyleSheet,
   Alert,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native'
-
-
-const People = [
-  {
-    name: 'john',
-    age: 20
-  },
-  {
-    name: 'peter',
-    age: 23
-  },
-  
-]
 
 type PersonProps = {
   name: string;
@@ -30,17 +18,37 @@ type PersonProps = {
 const Person = ({ name, age }: PersonProps) => (
   <View style={styles.item}>
     <Text style={styles.script}>My name is {name} and I'm {age} years old</Text>
+    <TouchableOpacity 
+      style={{backgroundColor:'red', width:60, height:40}}
+    >
+      <Text style={{fontSize:20}}>delete</Text>
+    </TouchableOpacity>
   </View>
 )
 
 function TabOneScreen({ navigation }: any) {
+
+  const [people,setPeople] = useState(
+    [
+      {
+        name: 'john',
+        age: 20
+      },
+      {
+        name: 'peter',
+        age: 23
+      },
+    ]
+  )
+
   const [name, onChangeName] = useState('');
   const [age, onChangeAge] = useState('');
 
   const addPerson = ()=> {
-    People.push({name:name, age:Number(age)})
-    console.log(JSON.stringify(People))
+    setPeople([...people, {name:name, age:Number(age)}])
   }
+
+  
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -64,7 +72,7 @@ function TabOneScreen({ navigation }: any) {
         
       </View>
       <FlatList
-        data={People}
+        data={people}
         renderItem={({ item }) => <Person name={item.name} age={item.age} />}
         keyExtractor={(item, index) => index.toString()}
       />
@@ -89,13 +97,12 @@ const styles = StyleSheet.create({
   script: {
     fontSize: 20
   },
-
   title: {
     fontSize: 30
   },
   input: {
     width: '50%',
-    height: 30,
+    height: 40,
     borderWidth: 1,
     margin: 3
   },
